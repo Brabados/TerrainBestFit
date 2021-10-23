50956ff9d7bd3ead00ea9998d2337bf2b791891b
Brabados <rhysipod@gmail.com>
2021-10-23 17:52:04 +1100

    Adding tests and begining to work on bug in regards to oriantation issue

---
+++ PlaneOfBestFitTest/BestFitTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+    [TestClass]
+    public class BestFitTest
+    {
+        [TestMethod]
+        public void CheckCoOrdinatesAreWorkingCorrectly()
+        {
+            int x = 55;
+            int y = 55;
+            int size = 10;
+
+        
+        }
+
+
+    }
+
